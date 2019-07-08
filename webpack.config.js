48faacd7684533a3b5d63db1be2a1f928bb33988
Ashley Heestand <heestand.ashley@gmail.com>
2019-07-08 10:28:55 -0500

    working on client side

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
-  mode: 'development',
+  entry: ['babel-polyfill', './client/index'], // assumes your entry point is the index.js in the root of your project folder
   output: {
-    path: __dirname + '/public', // assumes your bundle.js will also be in the root of your project folder
-    filename: 'bundle.js',
+    path: __dirname, // assumes your bundle.js will also be in the root of your project folder
+    filename: './public/bundle.js',
   },
+  mode: 'development',
   devtool: 'source-maps',
   module: {
     rules: [
       {
         test: /\.js$/,
+        include: resolve(__dirname, './client'),
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
