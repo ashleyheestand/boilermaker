84bbb78f1f20559cd678f44ba82736e3c00853c9
Ashley Heestand <heestand.ashley@gmail.com>
2019-07-10 10:28:58 -0500

    auth

--- server/index.js
+++ server/index.js
@@ -3,12 +3,42 @@ const app = express();
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+const passport = require('passport');
+
+dbStore.sync();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('./apiRoutes'));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'this is a secret',
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(done);
+});
 
 app.get('*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -20,8 +50,4 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500).send(err.message || 'Internal server error');
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+module.exports = app;
